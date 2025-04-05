409dcf346fdf908d7ed8dde829f0ba29439a22ec
PoornachanderReddy <poornachander295@gmail.com>
2025-04-05 21:31:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//your JS code here. If required.
+let angle = 0;
+      const line = document.getElementById("line");
+      setInterval(() => {
+        line.style.transform = `rotate(${angle++}deg) `;
+      }, 20);
